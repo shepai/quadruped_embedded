36a7c5e7cd987bfdb870a65420a04e93f6def0ad
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2026-02-12 17:48:16 +0000

    converter

--- Code/environment.py
+++ Code/environment.py
@@ -9,22 +9,19 @@ class environment:
         self.m = mujoco.MjModel.from_xml_path(path + "/quadruped_embedded/Model/quadruped_presstip.xml")
         self.data = mujoco.MjData(self.m)
     def step(self, target_angles):
-        kp = 20
-        kd = 2
-
         for i in range(12):
-            q = self.data.qpos[i]
-            qd = self.data.qvel[i]
-
-            torque = kp*(target_angles[i] - q) - kd*qd
-            self.data.ctrl[i] = torque
-
+            self.data.ctrl[i] = target_angles[i]
+        #print(self.data.ctrl)
         mujoco.mj_step(self.m, self.data)
+
     def observe(self): #gather observations
         pass 
 
 if __name__=="__main__":
     env=environment()
-    with viewer.launch(env.m, env.data) as v:
+    print("nu:", env.m.nu)
+    print("njnt:", env.m.njnt)
+    print("actuator mapping:", env.m.actuator_trnid)
+    with viewer.launch_passive(env.m, env.data) as v:
         while v.is_running():
-            env.step([0 for i in range(12)])
+            env.step([np.random.random() for _ in range(12)]) 
---
+++ Model/convert.py
@@ -0,0 +1,7 @@
+from urdf2mjcf import run
+
+run(
+    urdf_path="/its/home/drs25/Documents/GitHub/quadruped_embedded/Model/Quadruped_prestip.urdf",
+    mjcf_path="/its/home/drs25/Documents/GitHub/quadruped_embedded/Model/quadruped_presstip2.xml",
+    copy_meshes=True
+)
