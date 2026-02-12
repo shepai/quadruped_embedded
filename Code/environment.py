0a10cfb0946bc68a5985b081373d128c6bfdb656
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2026-02-12 14:53:51 +0000

    updates

--- Code/environment.py
+++ Code/environment.py
@@ -4,9 +4,27 @@ from mujoco import viewer
 import time
 import numpy as np
 
-m = mujoco.MjModel.from_xml_path("/its/home/drs25/Documents/GitHub/quadruped_embedded/Model/quadruped_presstip.xml")
-data = mujoco.MjData(m)
+class environment:
+    def __init__(self, path="/its/home/drs25/Documents/GitHub/"):
+        self.m = mujoco.MjModel.from_xml_path(path + "/quadruped_embedded/Model/quadruped_presstip.xml")
+        self.data = mujoco.MjData(self.m)
+    def step(self, target_angles):
+        kp = 20
+        kd = 2
 
-with viewer.launch(m, data) as v:
-    while v.is_running():
-        mujoco.mj_step(m, data)
+        for i in range(12):
+            q = self.data.qpos[i]
+            qd = self.data.qvel[i]
+
+            torque = kp*(target_angles[i] - q) - kd*qd
+            self.data.ctrl[i] = torque
+
+        mujoco.mj_step(self.m, self.data)
+    def observe(self): #gather observations
+        pass 
+
+if __name__=="__main__":
+    env=environment()
+    with viewer.launch(env.m, env.data) as v:
+        while v.is_running():
+            env.step([0 for i in range(12)])
